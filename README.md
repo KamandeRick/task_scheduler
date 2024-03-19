9b1272144c118ff9f9e8c4b92fc71b1a9e7c1487
kali4hckrs <kali4hckrs@kali>
2024-03-19 21:29:01 +0300

    add readme and file with necessary modules

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is meant as a practice project in preparation of the Airbnb clone project. 
---
+++ task_scheduler.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import cmd
+import unittest
+from datetime import datetime
+from io import StringIO
+import sys
